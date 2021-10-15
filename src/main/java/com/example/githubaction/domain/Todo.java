e9e994ca034e0215c97f7dd04c2ec5d6ac5c95c7
peter-alethio <peter@alethio.io>
2021-10-15 09:37:19 +0900

    feat: 제목 빈값 체크

--- src/main/java/com/example/githubaction/domain/Todo.java
+++ src/main/java/com/example/githubaction/domain/Todo.java
@@ -1,6 +1,7 @@
 package com.example.githubaction.domain;
 
 import lombok.EqualsAndHashCode;
+import org.apache.logging.log4j.util.Strings;
 
 @EqualsAndHashCode
 public class Todo {
@@ -8,6 +9,13 @@ public class Todo {
     private final String title;
 
     public Todo(String title) {
+        checkTitle(title);
         this.title = title;
+    }
+
+    private void checkTitle(String title) {
+        if (Strings.isEmpty(title)) {
+            throw new IllegalArgumentException();
+        }
     }
 }
--- src/test/java/com/example/githubaction/domain/TodoTest.java
+++ src/test/java/com/example/githubaction/domain/TodoTest.java
@@ -1,8 +1,11 @@
 package com.example.githubaction.domain;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 class TodoTest {
 
@@ -10,5 +13,12 @@ class TodoTest {
     void 생성() {
         Todo todo = new Todo("todo");
         assertThat(todo).isEqualTo(new Todo("todo"));
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    void 제목_빈값(String title) {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> new Todo(title));
     }
 }
